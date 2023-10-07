d6bedc0f678e85231056ed6ecb20b74693eb1905
abdelali <abdo@MacBook-Pro.local>
2023-10-07 01:37:46 +0100

    steps

--- app.vue
+++ app.vue
@@ -1,11 +1,44 @@
 <template>
-  <StepsContainer>
+  <StepsContainer ref="StepsContainerRef" :count="count">
+    <template #step1>
+      <div>
+        <h1>Step 1</h1>
+        <button @click="next">Next</button>
+    <button @click="prev">Prev</button>
+      </div>
+    </template>
+    <template #step2>
+      <h1>Step 2</h1>
+    </template>
+    <template #step3>
+      <h1>Step 3</h1>
+    </template>
+    <template #step4>
+      <h1>Step 3</h1>
+    </template>
+
+
     
   </StepsContainer>
+
+  
 </template>
 
 
 
 <script setup>
+ const count = 4; // number of steps
+
+
  import StepsContainer from '@/components/StepsContainer.vue';
+ const StepsContainerRef = ref(null);
+
+ const next = () => {
+  StepsContainerRef.value.nextStep();
+ }
+
+ const prev = () => {
+  StepsContainerRef.value.prevStep();
+ }
+
 </script>
--- components/StepsContainer.vue
+++ components/StepsContainer.vue
@@ -1,8 +1,117 @@
 <template>
-steps
+    <div class=" w-full h-screen flex">
+        <div class="m-auto">
+            <transition :name="animation">
+                <slot :name="currentStep"></slot>
+            </transition>
+        </div>
+
+      
+
+    </div>
 </template>
+  
 
 
 <script setup>
 
+const props = defineProps({
+    count: {
+        type: Number,
+        required: true,
+    },
+});
+
+const animation = ref('slide-up');
+const currentStep = ref('step1');
+const index = ref(1);
+const nextStep = () => {
+    animation.value = 'slide-up';
+    if (index.value < props.count) {
+        index.value++;
+        currentStep.value = `step${index.value}`;
+    }
+
+
+    
+   
+};
+
+const prevStep = () => {
+    animation.value = 'side-down';
+    if (index.value > 1) {
+        index.value--;
+        currentStep.value = `step${index.value}`;
+    }
+    
+
+    
+};
+
+//get slots number
+const slots = ref(0);
+
+onMounted(() => {
+});
+
+
+
+
+defineExpose({
+    nextStep,
+    prevStep,
+});
 </script>
+
+
+
+<style>
+.slide-up-enter-active {
+    transition: all 0.3s ease;
+}
+
+.slide-up-enter-from {
+    transform: translateY(100%);
+}
+
+.slide-up-enter-to {
+    transform: translateY(0);
+}
+
+.slide-up-leave-active {
+    transition: all 0.3s ease;
+}
+
+.slide-up-leave-from {
+    transform: translateY(0);
+}
+
+.slide-up-leave-to {
+    transform: translateY(-100%);
+}
+
+
+.side-down-enter-active {
+    transition: all 0.3s ease;
+}
+
+.side-down-enter-from {
+    transform: translateY(-100%);
+}
+
+.side-down-enter-to {
+    transform: translateY(0);
+}
+
+.side-down-leave-active {
+    transition: all 0.3s ease;
+}
+
+.side-down-leave-from {
+    transform: translateY(0);
+}
+
+.side-down-leave-to {
+    transform: translateY(100%);
+}
+</style>
